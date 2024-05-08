b93b6556073ea45aac682a540f86b3677fdd7806
shoaibmehmood-pursue <shoaib@pursue.today>
2024-05-08 16:24:15 +0500

    added otpless

--- components/ContactForm/index.vue
+++ components/ContactForm/index.vue
@@ -61,6 +61,15 @@ export default {
                 navElement.classList.remove('z-[1]');
             }
         }
+    },
+    mounted() {
+        const script = document.createElement('script');
+        script.id = 'otpless-sdk';
+        script.type = 'text/javascript';
+        script.src = 'https://otpless.com/v2/auth.js';
+        script.dataset.appid = 'UIY0M5J2HG8ASPBVY4SF';
+
+        document.body.appendChild(script);
     }
 }
 </script>
@@ -89,7 +98,8 @@ export default {
                 </header>
 
                 <section class="modal-body" id="modalDescription">
-                    <div v-if="!submitted" class="wrapper">
+                    <div id="otpless-login-page"></div>
+                    <!-- <div v-if="!submitted" class="wrapper">
                         <div class="contact-form-wrapper">
                             <div class="flex justify-center px-3 md:px-8">
                                 <div class="header-container w-full">
@@ -157,7 +167,7 @@ export default {
                             <svg class="text-emerald-500" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8m4.59-12.42L10 14.17l-2.59-2.58L6 13l4 4l8-8z"/></svg>
                             <h2 class="mt-8">Your message has been sent successfully!</h2>
                         </div>
-                    </div>
+                    </div> -->
                 </section>
             </div>
         </div>
